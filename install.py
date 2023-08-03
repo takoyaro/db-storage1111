c51a682264196d067aee85b840bd314af76976c5
96NekoRS <38354202+96NekoRS@users.noreply.github.com>
2023-08-03 20:54:17 +0200

    Fixed crash caused by lora_hashes

--- install.py
+++ install.py
@@ -2,3 +2,6 @@ import launch
 
 if not launch.is_installed("pymongo"):
     launch.run_pip("install pymongo", "requirements for db-storage1111")
+
+if not launch.is_installed("tssplit"):
+    launch.run_pip("install tssplit", "requirements for db-storage1111")
--- scripts/storage.py
+++ scripts/storage.py
@@ -5,6 +5,8 @@ import re
 import modules.scripts as scripts
 import gradio as gr
 from pymongo import MongoClient
+import shlex
+from tssplit import tssplit
 
 mongo_host = os.environ.get('DB_HOST', 'localhost')
 mongo_port = int(os.environ.get('DB_PORT', 27017))
@@ -20,6 +22,7 @@ def get_collection(database_name, collection_name):
     collection = db[collection_name]
     return collection
 
+
 class Scripts(scripts.Script):
     def title(self):
         return "Mongo Storage"
@@ -33,20 +36,20 @@ class Scripts(scripts.Script):
         collection_name = gr.inputs.Textbox(label="Collection Name", default="Automatic1111")
         return [checkbox_save_to_db, database_name, collection_name]
 
-    def postprocess(self, p, processed,checkbox_save_to_db,database_name,collection_name):
+    def postprocess(self, p, processed, checkbox_save_to_db, database_name, collection_name):
         collection = get_collection(database_name, collection_name) if checkbox_save_to_db else None
         if collection is None:
             return True
-        
-        for i in range(len(processed.images)):
 
+        for i in range(len(processed.images)):
             # Extract image information
             regex = r"Steps:.*$"
             seed = processed.seed
             prompt = processed.prompt
             neg_prompt = processed.negative_prompt
             info = re.findall(regex, processed.info, re.M)[0]
-            input_dict = dict(item.split(": ") for item in str(info).split(", "))
+            input_dict = dict(tssplit(item, quote='"', delimiter=':', trim=' ') for item in
+                              tssplit(info, quote='"', delimiter=',', trim=' ', quote_keep=True))
             steps = int(input_dict["Steps"])
             seed = int(input_dict["Seed"])
             sampler = input_dict["Sampler"]
@@ -54,6 +57,7 @@ class Scripts(scripts.Script):
             size = tuple(map(int, input_dict["Size"].split("x")))
             model_hash = input_dict["Model hash"]
             model = input_dict["Model"]
+            lora_hashes = input_dict["Lora hashes"]
 
             image = processed.images[i]
             buffer = BytesIO()
@@ -61,15 +65,16 @@ class Scripts(scripts.Script):
             image_bytes = buffer.getvalue()
 
             collection.insert_one({
-                "prompt": prompt, 
-                "negative_prompt": neg_prompt, 
-                "steps": int(steps), 
-                "seed": int(seed), 
+                "prompt": prompt,
+                "negative_prompt": neg_prompt,
+                "steps": int(steps),
+                "seed": int(seed),
                 "sampler": sampler,
-                "cfg_scale": float(cfg_scale), 
-                "size": size, 
-                "model_hash": model_hash, 
-                "model": model, 
+                "cfg_scale": float(cfg_scale),
+                "size": size,
+                "model_hash": model_hash,
+                "model": model,
+                "lora_hashes": lora_hashes,
                 "image": image_bytes
             })
         return True
